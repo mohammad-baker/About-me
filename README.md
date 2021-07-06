1412ae7d78c20664c6c00a040984e9d5b3e76ec8
mohammad <altaeabbaker@gmail.com>
2021-07-06 17:29:20 +0300

    add score

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # About-me
+## first 
+i make loop for loop and array and try to call all array in case the number was 0 
+## second
+i was stuck in how can use if statmen in for loop and i face anthor issue to make the break go out from two loops
+## 3
+i make ul unorederd list and orederd list and try to style them in css to see the bult and number
+## 4
+i use a way to make score in the correct answers 
+
+    if(asac ==='YES' || asac==='Y' ){
+        console.log('yes ',name, ' you are correct');
+        alert('yes '+name+ ' you are correct');
+    
+    }
+    else if(asac ==='NO' || asac==='N'){
+        console.log();('Unfortunately your answer is wrong');
+        alert(name +'Unfortunately your answer is wrong');
+    }
+    else{
+        alert('that not answer you should answer yes or no');
+        console.log('try something else');
+    }
+    //6th guess a number.
+     
+    let num = prompt('hey'+name+'can guess a number between (1 to -16) yuo four opportunities to get the correct answer');
+     num = parseInt(num)
+    if  (num == 10){
+        alert("these is the right answer"+num)
+         num = parseInt(num)
+        break;
+        
+    }
+    else if (num <= 5){
+        let num =prompt('the number'+num+'is too low you have'+ i +"to try")
+        console.log('the number'+num+'is too low you have'+ i +"to try");
+         num = parseInt(num)
+    }
+    else if  (num >10 && num < 16) {
+        let num =prompt('the number'+num+'is high that the you have'+ i +"to try")
+        console.log('the number'+num+'is low you have'+ i +"to try");
+         num = parseInt(num)
+    }
+    else if (num <10 && num > 5){
+        let num =prompt('the number'+num+'is low than the  you have'+ i +"to try")
+        console.log('the number'+num+'is low you have'+ i +"to try");
+         num = parseInt(num)
+    }
+    else if(num >= 16){
+        let num =prompt('the number'+num+'is too high you have'+ i +"to try")
+        console.log('the number'+num+'is too high you have'+ i +"to try");
+         num = parseInt(num)
+    }
+console.log(i);
--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 let name = prompt('what is you name ??')
 alert('welcome '+ name +' lets play a guessing game .')
 console.log('his name is ',name);
+let score = 0;
 //1 my name
 let me = prompt('Is my name Mohammad altaeab ??')
 me=me.toLowerCase();
 if(me ==='yes' || me ==='y' ){
     console.log('yes ',name, ' you are correct');
     alert('yes '+name+ ' you are correct');
-
+score++
 }
 else if(me ==='no' || me ==='n'){
     console.log('Unfortunately your answer is wrong');
@@ -24,7 +25,7 @@ study=study.toLowerCase();
 if(study ==='yes' || study ==='y' ){
     console.log('yes ',name, ' you are correct');
     alert('yes '+name+ ' you are correct I\'m mechanical engineer');
-
+score++
 }
 else if(study ==='no' || study ==='n'){
     console.log('Unfortunately your answer is wrong');
@@ -35,16 +36,17 @@ else{
     console.log('try something else');
 }
 //3my garduat 2021
-let grad = prompt('Did I graduate this year(2021)?? ??')
+let grad = prompt('Did I graduate lasr year?? ??')
 grad=grad.toUpperCase();
 if(grad ==='YES' || grad ==='Y' ){
-    console.log('yes ',name, ' you are correct');
-    alert('yes '+name+ ' you are correct');
+    console.log(name, ' Unfortunately your answer is wrong');
+    alert(name+ ' Unfortunately your answer is wrong');
 
 }
 else if(grad ==='NO' || grad ==='N'){
-    console.log('Unfortunately your answer is wrong');
-    alert(name +' Unfortunately I graduate in 2021 this year');
+    console.log('you are correct');
+    alert(name  +' you are correct');
+    score++
 }
 else{
     alert('that not answer you should answer yes or no');
@@ -56,6 +58,7 @@ solid=solid.toLowerCase();
 if(solid ==='yes' || solid==='y' ){
     console.log('yes ',name, ' you are correct');
     alert('yes '+name+ ' you are correct');
+    score++
 
 }
 else if(solid ==='no' || solid==='n'){
@@ -69,16 +72,86 @@ else{
 //5 acas 
 let asac = prompt('Did I take a programming course from ASAC ??')
 asac=asac.toUpperCase();
-if(asac ==='YES' || asac==='Y' ){
-    console.log('yes ',name, ' you are correct');
-    alert('yes '+name+ ' you are correct');
+    if(asac ==='YES' || asac==='Y' ){
+        console.log('yes ',name, ' you are correct');
+        alert('yes '+name+ ' you are correct');
+        score++
+    
+    }
+    else if(asac ==='NO' || asac==='N'){
+        console.log();('Unfortunately your answer is wrong');
+        alert(name +'Unfortunately your answer is wrong');
+    }
+    else{
+        alert('that not answer you should answer yes or no');
+        console.log('try something else');
+    }
+    //6th guess a number.
+     
+    let num = prompt('hey'+name+'can guess a number between (1 to -16) yuo four opportunities to get the correct answer');
+     num = parseInt(num)
+     
+    for (let i=3 ;i>0;i--) {
+           if  (num === 10){
+        alert("these is the right answer"+num)
+        score++;
+        break;
 
+    }    
+ else if(num <= 5){
+         num =prompt('the numberis too low, you have'+ i +"to try");
+        console.log('the number is too low, you have'+ i +"to try");
+ }
+    
+ else if  (num >10 && num < 16){
+         num =prompt('the number is high than,  you have'+ i +"to try");
+        console.log('the number is low you have'+ i +"to try");
+ }
+
+    
+  else if (num <10 && num > 5){
+         num =prompt('the number is low than,   you have'+ i +"to try");
+        console.log('the number is low you have'+ i +"to try");
+  }
+    
+ else if (num >= 16){
+         num =prompt('the number is too high ,you have'+ i +"to try");
+        console.log('the number is too high ,you have'+ i +"to try");
+ }
+console.log(i);
+num = parseInt(num)
+console.log(typeof num)
+switch(i){
+    case 1 :
+        alert('the right answer was 10')
+        break;
+   }
 }
-else if(asac ==='NO' || asac==='N'){
-    console.log();('Unfortunately your answer is wrong');
-    alert(name +'Unfortunately your answer is wrong');
+
+   let fo=prompt('guess the name of University in jordan ?')
+   let un=['just','University of Jordan','Hashemite','Yarmouk'];
+loop1:
+   for(let y=5;y>0;y--){
+loop2:
+    for(let x=0;x<un.length;x++){
+        console.log(un[x]);
+        if(un[x] == fo){
+            
+            alert("you are right")
+            score++
+            break loop1;
+        }
+    }
+    
+     fo=prompt('guess the name of University in jordan ? there left '+y+'try')
+     switch(y){
+        case 1:
+            for(let v=0;v<un.length;v++)
+            {
+                console.log(un[v]);
+                alert(un[v]);
+            }
+            break;
+    }
 }
-else{
-    alert('that not answer you should answer yes or no');
-    console.log('try something else');
-}
+alert('your score is '+score+'/7');
